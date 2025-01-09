f8a128237a68fa9583b2045aacb6abc7462fae6a
Myakhailo Nikolaiev <65964059+ab3MN@users.noreply.github.com>
2025-01-09 11:16:54 +0200

    Add base project config (#1)
    * feat:add base config
    
    * fix:fix eslint.config.js
    
    * refactor:remove path to eslint.csj
    
    * feat:implemented eslint,prettier,stylelint,husky config
    
    * feat:add images,api,logo and favicon;refactor:change title and favicon
    
    * refactor:add react version to eslint config
    
    * fix:fix all deplo's errors
    
    * feat:add import and jsxAlly plugins to eslint,add import-plugin-resolver,new rules to eslint.config.js
    
    * Fonts (#3)
    
    * add fonts
    
    * add fonts
    
    * add
    
    ---------
    
    Co-authored-by: Myakhailo Nikolaiev <65964059+ab3MN@users.noreply.github.com>
    
    * implement component for button (#4)
    
    Co-authored-by: Myakhailo Nikolaiev <65964059+ab3MN@users.noreply.github.com>
    
    * add icons (#5)
    
    * add icons
    
    * add icons corrected
    
    * feat:add base and reset style config;refactor:update ReadMe
    
    ---------
    
    Co-authored-by: natalia646 <75090544+natalia646@users.noreply.github.com>
    Co-authored-by: Oleksii Ivanyshyn <114085843+Eeoneishin@users.noreply.github.com>
    Co-authored-by: IlMarauder <ilya.morderer@gmail.com>

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import eslintPlugin from 'vite-plugin-eslint';
+import path from 'path';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return defineConfig({
+    plugins: [
+      react(),
+      tsconfigPaths(),
+      eslintPlugin({
+        cache: false,
+        include: ['src/**/*.ts', 'src/**/*.tsx'],
+        exclude: ['node_modules', 'dist'],
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
+    },
+    define: {
+      'process.env': env,
+    },
+    css: {
+      modules: {
+        localsConvention: 'camelCase',
+      },
+    },
+  });
+};
