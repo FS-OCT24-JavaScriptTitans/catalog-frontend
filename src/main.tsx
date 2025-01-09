6b1552603e1f597f5572d6e1995e454a4f8c7021
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-09 09:59:37 +0200

    add fonts

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
 import { App } from './App';
-
 import { persistor, store } from './redux/store';
+import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
