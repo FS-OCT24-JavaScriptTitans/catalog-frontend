52cd4e03538382a916f23bce249d1f23103f87f8
Myakhailo Nikolaiev <65964059+ab3MN@users.noreply.github.com>
2025-01-09 11:16:54 +0200

    Add base project config (#1)
    * feat:add base config
    
    * fix:fix eslint.config.js
    
    * refactor:remove path to eslint.csj
    
    * feat:implemented eslint,prettier,stylelint,husky config
    
    * feat:add images,api,logo and favicon;refactor:change title and favicon
    
    * refactor:add react version to eslint config
    
    * fix:fix all deplo's errors
    
    * feat:add import and jsxAlly plugins to eslint,add import-plugin-resolver,new rules to eslint.config.js
    
    * Fonts (#3)
    
    * add fonts
    
    * add fonts
    
    * add
    
    ---------
    
    Co-authored-by: Myakhailo Nikolaiev <65964059+ab3MN@users.noreply.github.com>
    
    * implement component for button (#4)
    
    Co-authored-by: Myakhailo Nikolaiev <65964059+ab3MN@users.noreply.github.com>
    
    * add icons (#5)
    
    * add icons
    
    * add icons corrected
    
    * feat:add base and reset style config;refactor:update ReadMe
    
    ---------
    
    Co-authored-by: natalia646 <75090544+natalia646@users.noreply.github.com>
    Co-authored-by: Oleksii Ivanyshyn <114085843+Eeoneishin@users.noreply.github.com>
    Co-authored-by: IlMarauder <ilya.morderer@gmail.com>

---
+++ src/UI/Button/Button.tsx
@@ -0,0 +1,31 @@
+ 
+import React, { useState } from 'react';
+import styles from './Button.module.scss';
+
+type Props = {
+  label: string;
+  secondaryLabel?: string;
+  onClick: () => void;
+  variant: 'primary' | 'selected';
+};
+
+export const Button: React.FC<Props> = ({ label, secondaryLabel, variant = 'primary' }) => {
+  const [isToggled, setIsToggled] = useState(false);
+
+  const handleClick = () => {
+    setIsToggled(!isToggled);
+  };
+
+  const buttonVariant = isToggled ? 'selected' : variant;
+
+  return (
+    <button
+      type='button'
+      className={`${styles.button} ${styles[`button--${buttonVariant}`]}`}
+      onClick={handleClick}
+      aria-pressed={isToggled}
+    >
+      {isToggled ? secondaryLabel : label}
+    </button>
+  );
+};
