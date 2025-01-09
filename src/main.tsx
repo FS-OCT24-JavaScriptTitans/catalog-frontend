0609345ede522babc8a24f33d577a4691d673d81
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-09 11:17:49 +0300

    Fonts (#3)
    * add fonts
    
    * add fonts
    
    * add
    
    ---------
    
    Co-authored-by: Myakhailo Nikolaiev <65964059+ab3MN@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 
 import { App } from './App';
 import { persistor, store } from './redux/store';
+import './styles/main.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
