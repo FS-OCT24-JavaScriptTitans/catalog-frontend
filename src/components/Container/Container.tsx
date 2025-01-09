870cdf4fa18ad26d44f62c823609eeb270f089bb
Oleksii Ivanyshyn <114085843+Eeoneishin@users.noreply.github.com>
2025-01-09 17:14:26 +0200

    implemented HomePage-Layout (#9)

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode } from 'react';
+
+import styles from './Container.module.scss';
+
+interface ContainerProps {
+  children: ReactNode;
+  className?: string;
+}
+
+const Container: React.FC<ContainerProps> = ({ children, className }) => (
+    <section className={`${styles.container} ${className || ''}`}>
+      <div className={styles.wrapper}>{children}</div>
+    </section>
+  );
+
+export default Container;
