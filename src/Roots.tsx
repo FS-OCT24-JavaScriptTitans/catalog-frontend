c873d86f2c8f6c8ca56f1096eddd6fb2f3e7ec3b
ab3MN <ab3mn3@gmail.com>
2025-01-09 18:26:57 +0200

    feat:implemented auth route

--- src/Roots.tsx
+++ src/Roots.tsx
@@ -7,6 +7,7 @@ import { Loader } from './components/Loader/Loader';
 import { PATH } from './constants/path';
 import SignInPage from './pages/SignInPage';
 import SignUpPage from './pages/SignUpPage';
+import AuthRoute from './routing/AuthRoute';
 
 export const Root = () => (
   <BrowserRouter>
@@ -23,14 +24,16 @@ export const Root = () => (
           />
         </Route>
 
-        <Route
-          path="/signin"
-          element={<SignInPage />}
-        />
-        <Route
-          path="/signup"
-          element={<SignUpPage />}
-        />
+        <Route element={<AuthRoute />}>
+          <Route
+            path={PATH.SIGN_IN}
+            element={<SignInPage />}
+          />
+          <Route
+            path={PATH.SIGN_UP}
+            element={<SignUpPage />}
+          />
+        </Route>
       </Routes>
     </Suspense>
   </BrowserRouter>
--- src/routing/AuthRoute.tsx
+++ src/routing/AuthRoute.tsx
@@ -1,15 +1,36 @@
-import { ReactNode } from 'react';
-import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { ReactNode, useLayoutEffect } from 'react';
+import { Navigate, Outlet, useLocation, useNavigate } from 'react-router-dom';
 
 import { PATH } from '@/constants/path';
 import { useAppSelector } from '@/redux/hooks';
+import useTokens from '@/hooks/useTokens';
+import { signInWithToken } from '@/api/firebase/signInWithToken';
+import useAuthData from '@/hooks/useAuthDats';
 
 const AuthRoute = (): ReactNode => {
   const location = useLocation();
+  const { getTokens } = useTokens();
+  const { refreshToken } = getTokens();
+  const { saveAuthData } = useAuthData();
+  const navigate = useNavigate();
   const user = useAppSelector((state) => state.user.user);
 
-  if ((user && location.pathname === PATH.SIGN_IN) || location.pathname === PATH.SIGN_UP) {
-    return <Navigate to={location.pathname} />;
+  useLayoutEffect(() => {
+    if (refreshToken && !user) {
+      signInWithToken(refreshToken)
+        .then((res) => {
+          if (res) {
+            saveAuthData(res);
+          }
+        })
+        .catch(() => {
+          navigate(PATH.SIGN_IN);
+        });
+    }
+  }, [refreshToken, user, saveAuthData, navigate]);
+
+  if (user && (location.pathname === PATH.SIGN_IN || location.pathname === PATH.SIGN_UP)) {
+    return <Navigate to={PATH.HOME} />;
   }
 
   return <Outlet />;
