71012880bd0772a0db874082f062c86bb539f967
ab3MN <ab3mn3@gmail.com>
2025-01-11 07:46:20 +0200

    refactor:remove unnecessary props from auth Form

--- src/UI/AuthForm/AuthForm.tsx
+++ src/UI/AuthForm/AuthForm.tsx
@@ -6,12 +6,9 @@ import { Button } from '../Button/Button';
 import s from './Auth.module.scss';
 
 import { Input } from '@/UI/Input/Input';
-// import { CustomLink } from '@/UI/Link/Link';
-import { PATH } from '@/constants/path';
 import GoogleButton from '@/components/Auth/GoogleButton';
 
 interface AuthFormProps<T extends FieldValues> {
-  title: string;
   fields: Array<{
     label: string;
     type: string;
@@ -21,25 +18,19 @@ interface AuthFormProps<T extends FieldValues> {
   register: UseFormRegister<T>;
   errors: Record<string, { message?: string }>;
   isValid: boolean;
-  linkPath: PATH;
-  linkLabel: string;
   buttonLabel: string;
 }
 
 export const AuthForm = <T extends FieldValues>({
-  // title,
   fields,
   onSubmit,
   register,
   errors,
   isValid,
-  // linkPath,
-  // linkLabel,
   buttonLabel,
 }: AuthFormProps<T>) => (
   <article className={s.auth}>
     <div className={s.container}>
-      {/* <h2 className={s.title}>{title}</h2> */}
       <form
         className={s.form}
         onSubmit={onSubmit}
@@ -63,13 +54,6 @@ export const AuthForm = <T extends FieldValues>({
       </form>
 
       <GoogleButton />
-
-      {/* <span>OR</span>
-
-      <CustomLink
-        path={linkPath}
-        label={linkLabel}
-      /> */}
     </div>
   </article>
 );
