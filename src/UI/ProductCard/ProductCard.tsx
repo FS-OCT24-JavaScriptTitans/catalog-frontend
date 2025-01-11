bbd2adf24d866c730135122de65eb8eb43a5c408
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-01-11 08:19:28 +0200

    Card layout without new constants

--- src/UI/ProductCard/ProductCard.tsx
+++ src/UI/ProductCard/ProductCard.tsx
@@ -2,89 +2,51 @@ import React from 'react';
 
 import srcImg from '../../../public/img/phones/apple-iphone-11/black/00.webp';
 import { Button } from '../Button/Button';
+import { IconButton } from '../IconButton/IconButton';
 import { Specifications } from '../Specifications/Specifications';
 
 import styles from './ProductCard.module.scss';
 
 // type Props = {
+//   id: string
 //   image: string;
 //   title: string;
 //   price: number;
 //   fullPrice?: number;
-//   screen: string;
-//   capacity: string;
-//   ram: string;
 // }
 
-// export const ProductCard: React.FC<Props> = ({
-//   image,
-//   title,
-//   price,
-//   fullPrice?,
-//   screen,
-//   capacity,
-//   ram,
-// }) => (
-//   <>
-//     <h1>ProductCard</h1>
-//     <div className={styles.card}>
-//       <img
-//         src={image}
-//         alt="Product image"
-//         className={styles.card__image}
-//       />
-//       <p className={styles.card__title}>Apple iPhone Xs 64GB Silver (iMT9G2FS/A)</p>
-//       <div className={styles.card__price}>
-//       <span className={styles.card__value}>${price}</span>
-//       {fullPrice && (
-//         <span className={styles.card__old_value}>${fullPrice}</span>
-//       )}
-//     </div>
-//     <div className={styles.create_line}></div>
-//     <div className={styles.specifications}>
-//       <div className={styles.spec_item}>
-//         <span className={styles.spec_name}>Screen</span>
-//         <span className={styles.spec_value}>{screen}</span>
-//       </div>
-//       <div className={styles.spec_item}>
-//         <span className={styles.spec_name}>Capacity</span>
-//         <span className={styles.spec_value}>{capacity}</span>
-//       </div>
-//       <div className={styles.spec_item}>
-//         <span className={styles.spec_name}>RAM</span>
-//         <span className={styles.spec_value}>{ram}</span>
-//       </div>
-//     </div>
-//       <div className="actions">
-//         <Button
-//           label="Add to cart"
-//           secondaryLabel="Added"
-//         />
-//       </div>
-//     </div>
-//   </>
-// );
-export const ProductCard: React.FC = () => (
-  <article className={styles.card}>
-    <div className={styles.container}>
-      <img
-        src={srcImg}
-        alt="Product image"
-        className={styles.card__image}
-      />
-      <p className={styles.card__title}>Apple iPhone Xs 64GB Silver (iMT9G2FS/A)</p>
-      <div className={styles.card__price}>
-        <span className={styles.card__value}>$799</span>
-        <span className={styles.card__old_value}>$899</span>
-      </div>
-      <div className={styles.create_line}></div>
-      <Specifications />
-      <div className="actions">
-        <Button
-          label="Add to cart"
-          secondaryLabel="Added"
+export const ProductCard: React.FC = () => {
+  const product = {
+    id: 1,
+    imageSrc: srcImg,
+    title: 'Apple iPhone Xs 64GB Silver (iMT9G2FS/A)',
+    price: 799,
+    fullPrice: 899,
+  };
+
+  return (
+    <article className={styles.card}>
+      <div className={styles.container}>
+        <img
+          src={product.imageSrc}
+          alt="Product image"
+          className={styles.card__image}
         />
+        <a className={`${styles.card__title} primary-text`}>Apple iPhone Xs 64GB Silver (iMT9G2FS/A)</a>
+        <div className={styles.card__price}>
+          <span className={styles.card__value}>${product.price}</span>
+          {product.fullPrice && <span className={styles.card__old_value}>${product.fullPrice}</span>}
+        </div>
+        <div className={styles.create_line}></div>
+        <Specifications />
+        <div className={styles.actions}>
+          <Button
+            label="Add to cart"
+            secondaryLabel="Added"
+          />
+          <IconButton></IconButton>
+        </div>
       </div>
-    </div>
-  </article>
-);
+    </article>
+  );
+};
