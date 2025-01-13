d6d115cfe7e09bfe9e8d3b7fc97d34657501b3cb
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-01-13 22:26:50 +0200

    create footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,10 +4,32 @@ import Logo from '../Logo/Logo';
 
 import s from './Footer.module.scss';
 
+import NavigationLink from '@/UI/NavLink/NavigationLink';
+
 export const Footer: React.FC = () => (
   <footer className={s.footer}>
-    <span className={s.logo}>
-      <Logo />
-    </span>
+    <div className={s.create_line}></div>
+    <div className={s.container}>
+      <span className={s.logo}>
+        <Logo />
+      </span>
+      <nav className={s.links}>
+        <NavigationLink
+          to="#"
+          label="Github"
+          className={s.link}
+        />
+        <NavigationLink
+          to="#"
+          label="Contacts"
+          className={s.link}
+        />
+        <NavigationLink
+          to="#"
+          label="Rights"
+          className={s.link}
+        />
+      </nav>
+    </div>
   </footer>
 );
