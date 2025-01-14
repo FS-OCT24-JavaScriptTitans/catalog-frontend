e141805cd4d68ddaa2b8b43cbfa89234fef0f0ae
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-14 15:16:10 +0200

    displays favorites product with redux

---
+++ src/components/Favorites/Favorites.tsx
@@ -0,0 +1,32 @@
+import { shallowEqual } from 'react-redux';
+
+import s from './Favorites.module.scss';
+
+import { useAppSelector } from '@/redux/hooks';
+import { selectFavorites } from '@/redux/selectors';
+import { ProductCard } from '@/UI/ProductCard/ProductCard';
+import EmptyCart from '@/UI/EmptyCart/EmptyCart';
+
+export const Favourites = () => {
+  const favorites = useAppSelector(selectFavorites, shallowEqual);
+  const favoritesLength = favorites.length;
+
+  return (
+    <section className={s.contaiter}>
+      <h2 className={s.title}>Favorites</h2>
+      {favoritesLength ?
+        <>
+          <span className={s.contaiter__text}>{favoritesLength} items</span>
+          <section className={s.products}>
+            {favorites.map((product) => (
+              <ProductCard
+                key={product.id}
+                product={product}
+              />
+            ))}
+          </section>
+        </>
+      : <EmptyCart />}
+    </section>
+  );
+};
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -5,18 +5,26 @@ import storage from 'redux-persist/lib/storage';
 
 import userSlice from './slices/user/userSlice';
 import cartReducer, { CartState } from './slices/cart/carrt.slice';
+import favouriteReducer from './slices/favorites/favorites.slice';
 
 const persistConfig = {
   key: 'cart',
   storage,
 };
 
+const favouritePersistConfig = {
+  key: 'favourites',
+  storage,
+};
+
 const persistedCartReducer = persistReducer<CartState>(persistConfig, cartReducer);
+const persistedFavouriteReducer = persistReducer(favouritePersistConfig, favouriteReducer);
 
 export const store = configureStore({
   reducer: {
     cart: persistedCartReducer,
     user: userSlice,
+    favorites: persistedFavouriteReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
