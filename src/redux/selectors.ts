6e531f5acdd239124e259afabf80f788786272f9
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-15 11:10:57 +0200

    Favorites page (#37)
    * displays favorites product with redux
    
    * fix styles
    
    * fix navigation component
    
    * add red heart as favorite
    
    * fix favorite icon and rename reduser

--- src/redux/selectors.ts
+++ src/redux/selectors.ts
@@ -2,3 +2,4 @@ import { RootState } from './store';
 
 export const selectUser = (state: RootState) => state.user.user;
 export const selectCart = (state: RootState) => state.cart.cart;
+export const selectFavorites = (state: RootState) => state.favorites.favorites;
