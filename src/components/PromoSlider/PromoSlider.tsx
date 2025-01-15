3f58c60cb50c1f37fd0a4ab04f202a04b41d9c96
Oleksii Ivanyshyn <114085843+Eeoneishin@users.noreply.github.com>
2025-01-15 11:00:01 +0200

    Feature/86c1j3gha/promo slider 2 (#39)
    * saving the slider for further pull
    
    * saving the slider for further pull
    
    * implement slider for first section in HomePage
    
    * add mixine:flex-container

---
+++ src/components/PromoSlider/PromoSlider.tsx
@@ -0,0 +1,105 @@
+import React, { useState, useRef } from 'react';
+import { Swiper as SwiperClass } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination, Navigation } from 'swiper/modules';
+import { useNavigate } from 'react-router-dom';
+
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import styles from './PromoSlider.module.scss';
+
+import { Arrow } from '@/UI/Arrow/Arrow';
+
+const slides = [
+  {
+    img: 'public/img/slides/banner-phones.png',
+    link: '/phones',
+  },
+  {
+    img: 'public/img/slides/banner-tablets.png',
+    link: '/tablets',
+  },
+  {
+    img: 'public/img/slides/banner-accessories.png',
+    link: '/accessories',
+  },
+];
+
+const PromoSlider: React.FC = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+  const swiperRef = useRef<SwiperClass | null>(null);
+  const navigate = useNavigate();
+
+  const handleBulletClick = (index: number) => {
+    if (swiperRef.current) {
+      swiperRef.current.slideToLoop(index);
+      setActiveIndex(index);
+    }
+  };
+
+  const handleSlideClick = (link: string) => {
+    navigate(link);
+  };
+
+  return (
+    <>
+      <div>
+        <div className={styles.sliderContainer}>
+          <Swiper
+            modules={[Pagination, Navigation]}
+            spaceBetween={100}
+            slidesPerView={1}
+            loop
+            navigation={{
+              nextEl: `.${styles.nextButton}`,
+              prevEl: `.${styles.prevButton}`,
+            }}
+            onSwiper={(swiper) => {
+              swiperRef.current = swiper;
+            }}
+            onSlideChange={(swiper) => setActiveIndex(swiper.realIndex)}
+            pagination={false}
+          >
+            {slides.map((slide, index) => (
+              <SwiperSlide
+                key={index}
+                className={styles.swiperSlide}
+              >
+                <div
+                  className={styles.imageContainer}
+                  onClick={() => handleSlideClick(slide.link)}
+                >
+                  <img
+                    src={slide.img}
+                    alt={`Slide ${index + 1}`}
+                    className={styles.image}
+                  />
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+
+        <div className={styles.paginationWrapper}>
+          {slides.map((_, index) => (
+            <div
+              key={index}
+              className={`${styles.bullet} ${index === activeIndex ? styles.active : ''}`}
+              onClick={() => handleBulletClick(index)}
+            ></div>
+          ))}
+        </div>
+      </div>
+
+      <div className={styles.prevButton}>
+        <Arrow direction={'left'} />
+      </div>
+      <div className={styles.nextButton}>
+        <Arrow direction={'rigth'} />
+      </div>
+    </>
+  );
+};
+
+export default PromoSlider;
