3357fffd4ee249767cb2bae4c9b40a684090af2b
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-15 13:45:02 +0200

    implement theme switcher

--- src/components/ThemeSwitcher/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher/ThemeSwitcher.tsx
@@ -1,13 +1,38 @@
+import { useEffect, useState } from 'react';
+
 import s from './ThemeSwitcher.module.scss';
 
-export const ThemeSwitcher = () => (
+import useLocaLStorage from '@/hooks/useLocaLStorage';
+
+const DARK = 'dark';
+
+export const ThemeSwitcher = () => {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const { setItem, getItem, removeItem } = useLocaLStorage('theme');
+  const root = document.querySelector('#root');
+
+  useEffect(() => {
+    const theme = getItem();
+
+    if (theme) {
+      root?.classList.remove(DARK);
+      removeItem();
+    } else {
+      root?.classList.add(DARK);
+      setItem(DARK);
+    }
+  }, [getItem, isDarkTheme, removeItem, root?.classList, setItem]);
+
+  return (
     <span className={s.container}>
       <label className={s.switch}>
         <input
           type="checkbox"
           className={s.input}
+          onChange={() => setIsDarkTheme(!isDarkTheme)}
         />
         <span className={s.slider}></span>
       </label>
     </span>
   );
+};
