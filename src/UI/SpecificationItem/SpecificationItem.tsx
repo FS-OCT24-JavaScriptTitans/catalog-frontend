86a22df3401aedd560cfc96421160255d876fbe9
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-01-16 21:46:47 +0200

    fixed product card

--- src/UI/SpecificationItem/SpecificationItem.tsx
+++ src/UI/SpecificationItem/SpecificationItem.tsx
@@ -9,7 +9,9 @@ interface Props {
 
 export const SpecificationItem: React.FC<Props> = ({ name, value }) => (
   <div className={styles.spec_item}>
-    <span className={`${styles.spec_name} small-text`}>{name}</span>
-    <span className={`${styles.spec_value} small-text`}>{value}</span>
+    <span className={`${styles.spec_name} small-text`}>{name.charAt(0).toUpperCase() + name.slice(1)}</span>
+    <span className={`${styles.spec_value} small-text`}>
+      {value.split(' ').slice(0, 2).join(' ') + (value.split(' ').length > 2 ? ' ...' : '')}
+    </span>
   </div>
 );
