0a5852ed55af44ca88ee101e3c088eb0960d6207
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-17 09:58:55 +0200

    Fix favorite page (#54)
    * fix unfavorite click
    
    * fix favorites
    
    * fix favorites
    
    * add empty png and red-fav icon
    
    * fix logo
    
    * add allow scroll to nav
    
    * fix nav menu
    
    * fix position

--- src/components/Favorites/Favorites.tsx
+++ src/components/Favorites/Favorites.tsx
@@ -12,28 +12,23 @@ export const Favourites = () => {
   const favoritesLength = favorites.length;
 
   return (
-    <section className="section">
-      <h2 className={s.title}>Favorites</h2>
+    <section className={s.contaiter}>
       {favoritesLength ?
         <>
+          <h2 className={s.title}>Favorites</h2>
           <span className={s.contaiter__text}>{favoritesLength} items</span>
           <section className={s.products}>
             {favorites.map((product) => (
-              <span
+              <ProductCard
                 key={product.id}
-                className={s.product}
-              >
-                <ProductCard
-                  key={product.id}
-                  product={product}
-                />
-              </span>
+                product={product}
+              />
             ))}
           </section>
         </>
       : <EmptyContainer
           title="Theris no favoritess"
-          pathToImg="/img/cart-is-empty.png"
+          pathToImg="/img/empty-fav-page.png"
           alt="empty-favorites"
         />
       }
