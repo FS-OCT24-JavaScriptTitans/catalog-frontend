d2fe8da1d987db3fac7a3db297c0ba9186767e5e
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-20 13:53:31 +0200

    created not found page

---
+++ src/pages/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,11 @@
+import EmptyContainer from '@/UI/EmptyContainer/EmptyContainer';
+
+const NotFoundPage = () => (
+  <EmptyContainer
+    title={'Page not found'}
+    pathToImg={'../../../public/img/not-found-page.png'}
+    alt={'not found page'}
+  />
+);
+
+export default NotFoundPage;
