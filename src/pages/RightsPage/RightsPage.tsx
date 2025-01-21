5950ee7029d54a4d0c9d0e94fc109f40d072b6de
ab3MN <ab3mn3@gmail.com>
2025-01-21 08:15:03 +0200

    feat:add translate to title

--- src/pages/RightsPage/RightsPage.tsx
+++ src/pages/RightsPage/RightsPage.tsx
@@ -1,21 +1,27 @@
+import { useTranslation } from 'react-i18next';
+
 import s from './RightsPage.module.scss';
 
 import { Rights } from '@/components/Rights/Rights';
 import { Container } from '@/UI/Container/Container';
 import EarthScene from '@/UI/EarthScene/EarthScene';
 
-const RightsPage = () => (
-  <Container
-    title="Rights"
-    containerMb="10px"
-  >
-    <div className={s.wrapper}>
-      <EarthScene />
-      <Rights />
-    </div>
+const RightsPage = () => {
+  const { t } = useTranslation();
 
-    <p className={s.year}>© 2025</p>
-  </Container>
-);
+  return (
+    <Container
+      title={t('rights.title')}
+      containerMb="10px"
+    >
+      <div className={s.wrapper}>
+        <EarthScene />
+        <Rights />
+      </div>
+
+      <p className={s.year}>© 2025</p>
+    </Container>
+  );
+};
 
 export default RightsPage;
