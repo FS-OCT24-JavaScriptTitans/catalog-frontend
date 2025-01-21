e03b4274087c1492542c6b1fb31d979905817377
Ilya Morderer <ilya.morderer@gmail.com>
2025-01-21 20:10:42 +0200

    Product page adding swiper on photos and fixing back  button

--- src/components/Product/ProductPhotos/ProductPhotos.tsx
+++ src/components/Product/ProductPhotos/ProductPhotos.tsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import cn from 'classnames';
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
+import { Navigation } from 'swiper/modules';
 
 import styles from './ProductPhotos.module.scss';
 
 import { Product } from '@/types/Product.type';
+
+import 'swiper/css/navigation';
 
 type Props = {
   product: Product;
@@ -11,6 +15,13 @@ type Props = {
 
 const ProductPhotos: React.FC<Props> = ({ product }) => {
   const [selectedImage, setSelectedImage] = useState(product?.images[0]);
+  const swiperRef = useRef<SwiperClass | null>(null);
+
+  const handleSlideChange = (swiper: SwiperClass) => {
+    const currentIndex = swiper.activeIndex;
+
+    setSelectedImage(product.images[currentIndex]);
+  };
 
   useEffect(() => {
     setSelectedImage(product?.images[0]);
@@ -19,11 +30,35 @@ const ProductPhotos: React.FC<Props> = ({ product }) => {
   return (
     <>
       <div className={styles.mainImageBox}>
-        <img
+        <Swiper
+          modules={[Navigation]}
+          spaceBetween={100}
+          slidesPerView={1}
+          navigation={{
+            nextEl: `.${styles.nextButton}`,
+            prevEl: `.${styles.prevButton}`,
+          }}
+          onSwiper={(swiper) => {
+            swiperRef.current = swiper;
+          }}
+          onSlideChange={handleSlideChange}
+          pagination={false}
+        >
+          {product.images.map((_, index) => (
+            <SwiperSlide key={index}>
+              <img
+                src={`/${selectedImage}`}
+                alt="product image"
+                className={styles.mainPhoto}
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+        {/* <img
           src={`/${selectedImage}`}
           alt="product image"
           className={styles.mainPhoto}
-        />
+        /> */}
       </div>
 
       <div className={styles.imagesContainer}>
