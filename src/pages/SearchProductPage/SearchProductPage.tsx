2d828cc959a2c7fa334bb48b8d9d82a3f2fa2dd4
Oleksandr Andrushchenko <aandrushchenko1402@gmail.com>
2025-01-21 17:49:54 +0200

    pagination on searchpage (#94)

--- src/pages/SearchProductPage/SearchProductPage.tsx
+++ src/pages/SearchProductPage/SearchProductPage.tsx
@@ -9,14 +9,18 @@ import { Loader } from '@/components/Loader/Loader';
 import EmptyContainer from '@/UI/EmptyContainer/EmptyContainer';
 import { useLanguage } from '@/hooks/useLanguage';
 import { LANGUAGE } from '@/constants/language';
+import Pagination from '@/components/Pagination/Pagination';
 
 const SearchProductPage = () => {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setLoading] = useState(true);
-  const [searchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
   const query = searchParams.get('query') || '';
   const { getLanguage } = useLanguage();
   const language = getLanguage() as LANGUAGE;
+  const pageNumber = Number(searchParams.get('page')) || 1;
+
+  const perPageParam = 16;
 
   useEffect(() => {
     getAllProducts(language)
@@ -28,6 +32,20 @@ const SearchProductPage = () => {
       .catch(() => setProducts([]))
       .finally(() => setLoading(false));
   }, [query, language]);
+
+  const handlePageChange = ({ selected }: { selected: number }) => {
+    setSearchParams({ query, page: (selected + 1).toString() });
+    window.scrollTo(0, 0);
+  };
+
+  const getPaginatedProducts = () => {
+    const offset = (pageNumber - 1) * perPageParam;
+
+    return [...products].slice(offset, offset + perPageParam);
+  };
+
+  const productsOnPage = getPaginatedProducts();
+  const pagesCount = Math.ceil(products.length / perPageParam);
 
   const getView = () => {
     switch (true) {
@@ -44,7 +62,19 @@ const SearchProductPage = () => {
         );
 
       default:
-        return <SearchProduct products={products} />;
+        return (
+          <>
+            <SearchProduct products={productsOnPage} />
+            <Pagination
+              forcePage={pageNumber - 1}
+              pageCount={pagesCount}
+              onChange={handlePageChange}
+              pageRangeDisplayed={1}
+              marginPagesDisplayed={1}
+            />
+            ;
+          </>
+        );
     }
   };
 
