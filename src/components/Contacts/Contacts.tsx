08d165c52a510302f6952c49a598fdafb4fe9c6e
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-21 17:13:11 +0200

    added translate

--- src/components/Contacts/Contacts.tsx
+++ src/components/Contacts/Contacts.tsx
@@ -1,3 +1,5 @@
+import { useTranslation } from 'react-i18next';
+
 import { ContactCard } from '../../components/Contacts/ContactCard/ContactCard';
 
 import s from './Contacts.module.scss';
@@ -5,15 +7,19 @@ import s from './Contacts.module.scss';
 import { Container } from '@/UI/Container/Container';
 import { developers } from '@/constants/developers';
 
-export const Contacts = () => (
-  <Container title={'Contacts'}>
-    <section className={s.container}>
-      {developers.map((developer) => (
-        <ContactCard
-          key={developer.name}
-          developer={developer}
-        />
-      ))}
-    </section>
-  </Container>
-);
+export const Contacts = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Container title={t('footer.contacts')}>
+      <section className={s.container}>
+        {developers.map((developer) => (
+          <ContactCard
+            key={developer.name}
+            developer={developer}
+          />
+        ))}
+      </section>
+    </Container>
+  );
+};
