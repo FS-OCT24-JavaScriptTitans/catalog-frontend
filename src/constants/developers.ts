cd5441dfbb70a06cb47b8ea37ebb62f412692f6a
natalia646 <75090544+natalia646@users.noreply.github.com>
2025-01-21 11:54:48 +0200

    add desc

--- src/constants/developers.ts
+++ src/constants/developers.ts
@@ -17,13 +17,13 @@ export const developers: Contact[] = [
     name: 'Nataliia Zaiats',
     photo: '../../../public/img/developers/Zaiats Natalia.jpg',
     github: 'https://github.com/natalia646',
-    desc: '',
+    desc: "I'm a front-end developer with hands-on experience in creating modern and responsive web applications. Skilled in HTML, CSS, JavaScript, React, and Next.js. I have a strong passion for delivering efficient, maintainable solutions and constantly strive to improve my skills. My goal is to contribute to impactful projects and grow as a professional in the tech industry.",
   },
   {
     name: 'Oleksandr Andrushchenko',
     photo: '../../../public/img/developers/Oleksandr Andrushchenko.png',
     github: 'https://github.com/andrushchenkoo',
-    desc: '',
+    desc: 'Frontend developer with experience in creating modern and interactive web applications. Proficient in HTML, CSS, JavaScript, and the React framework. Experienced in responsive design, website performance optimization, and API integration. Goal-oriented to create user-friendly and efficient interfaces.',
   },
   {
     name: 'Illia Morderer',
