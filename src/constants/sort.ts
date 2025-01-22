6ee0ada6172cee30a23395d63fb6c7ecfa5ba5c3
Myakhailo Nikolaiev <65964059+ab3MN@users.noreply.github.com>
2025-01-22 09:23:03 +0200

    refactor:products sort by price and new models (#102)

---
+++ src/constants/sort.ts
@@ -0,0 +1,5 @@
+export enum Sort {
+  Popularity = 'popularity',
+  HighToLow = 'high-to-low',
+  LowToHigh = 'low-to-high',
+}
--- src/utils/products/sortProducts.ts
+++ src/utils/products/sortProducts.ts
@@ -1,7 +1,8 @@
+import { Sort } from '@/constants/sort';
 import { Product } from '@/types/Product.type';
 
 export const sortNewModal = (products: Product[]): Product[] =>
-  products.reduceRight<Product[]>((acc, product) => {
+  [...products].reduceRight<Product[]>((acc, product) => {
     if (product.priceDiscount < product.priceRegular) {
       acc.push(product);
     }
@@ -11,3 +12,23 @@ export const sortNewModal = (products: Product[]): Product[] =>
 
 export const sortByHotPrice = (products: Product[]): Product[] =>
   [...products].sort((p1, p2) => p2.priceRegular - p2.priceDiscount - (p1.priceRegular - p1.priceDiscount));
+
+export const sortByPrice = (products: Product[], type: Sort): Product[] =>
+  [...products].sort((a, b) =>
+    type === Sort.HighToLow ? a.priceRegular - b.priceRegular : b.priceRegular - a.priceRegular,
+  );
+
+export const sortByPopularity = (products: Product[]) => [...products].sort(() => Math.random() - 0.5);
+
+export const sortProducts = (products: Product[], type: Sort) => {
+  switch (type) {
+    case Sort.Popularity:
+      return sortByPopularity(products);
+    case Sort.HighToLow:
+      return sortByPrice(products, type);
+    case Sort.LowToHigh:
+      return sortByPrice(products, type);
+    default:
+      return products;
+  }
+};
